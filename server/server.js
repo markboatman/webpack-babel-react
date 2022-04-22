ac8b57b662d909258a2bab4c804a336f4ede4f9f
Mark Boatman <markjboat@yahoo.com>
2022-04-22 16:36:00 +0800

    Added access to process.env.PORT in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 // get an instance of express
 const app = express();
 // build the path, join puts in the '/' for us
